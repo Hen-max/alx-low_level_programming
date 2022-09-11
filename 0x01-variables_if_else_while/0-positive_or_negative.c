2f339fb6d8787b9f761a5e451aa8faad63f17eec
okereke kelechi <henryokereke300@gmail.com>
2022-09-11 14:52:24 -0700

    Task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,18 +13,17 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/your code goes there/
-		if (n > 0)
-		{
-			printf("%i is positive\n", n);
-		}
-		else if (n == 0)
-		{
-			printf("%i is zero\n", n);
-		}
-		else if (n < 0)
-		{
-			printf("%i is negative\n", n);
-		}
-	return (0);
+	/* your code goes there */
+	if (n > 0)
+	{
+		printf("%i is positive\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%i is zero\n", n);
+	}
+	else if (n < 0)
+	{
+		printf("%i is negative\n", n);
+	}return (0);
 }
