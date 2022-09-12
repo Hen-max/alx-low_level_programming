74508c65a275afa51fc8277e138639bb6c70a1d3
okereke kelechi <henryokereke300@gmail.com>
2022-09-12 14:47:05 -0700

    Task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,11 +9,13 @@ int main(void)
 	int f_d;
 	int m_d;
 	int l_d;
+
 	while (c <= 999)
 	{
 		f_d = (c / 100 + '0');
 		m_d = (c / 10 % 10 + '0');
 		l_d = (c % 10 + '0');
+
 		if ((f_d < m_d) && (m_d < l_d))
 		{
 			putchar(f_d);
@@ -32,3 +34,4 @@ int main(void)
 	putchar('\n');
 	return (0);
 }
+
