329500debc4384eed81a82590747ad3bcde2801c
okereke kelechi <henryokereke300@gmail.com>
2022-09-14 21:29:49 -0700

    2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,17 +1,20 @@
 #include"main.h"
 
 /**
- * print_alphabet_x10-funtion that will print the alphabet 10 times
+ * print_alphabet_x10-funtion that will print the alphabet 10 times* 
  * Return: 0
  */
 
 void print_alphabet_x10(void)
+
 {
 
 	int line,ch;
 
 	for (line =0; line<=9;++line)
+
 	{
+		
 		for (ch ='a';ch<='z';++ch)
 
 			_putchar(ch);
