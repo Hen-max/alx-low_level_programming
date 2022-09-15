df7b4e615a8f1d832b573a880b626161e8f61d85
okereke kelechi <henryokereke300@gmail.com>
2022-09-14 21:10:37 -0700

    9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -17,6 +17,7 @@ void times_table(void)
 	{
 
 		_putchar(48);
+
 		for (mult = 1; mult <= 9; ++mult)
 		{
 
@@ -30,12 +31,14 @@ void times_table(void)
 			 * place the first digit if its two numbers
 			 */
 			if (prod <= 9)
+
 				_putchar(' ');
 			
 			else
 				_putchar((prod / 10) + 48); /*get the first digit*/
 			_putchar((prod % 10) + 48); /*get the second digit*/
 		}
+		
 		_putchar('\n');
 
 	}
