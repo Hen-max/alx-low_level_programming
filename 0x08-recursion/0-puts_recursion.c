2b20f106b98c748ba1d18087edad6ea05950984a
okereke kelechi <henryokereke300@gmail.com>
2022-09-29 03:38:09 -0700

    0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* _puts_recursion - print's a string followed by a new line
+* _puts_recursion - prints's a string followed by a new line
 * @s: string to be printed
 *
 * Return: void
@@ -12,10 +12,10 @@ void _puts_recursion(char *s)
 if (*s == '\0')
 {
 _putchar('\n');
-
 }
 else
 {
 _putchar(s[0]);
 _puts_recursion(s + 1);
+}
 }
